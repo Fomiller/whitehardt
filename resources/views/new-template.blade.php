7c1e4ebc3b4609361f94e98cad0a236467482853
Forrest <Forrestmillerj@gmail.com>
2020-07-13 19:23:17 -0500

    created template files

--- resources/views/new-template.blade.php
+++ resources/views/new-template.blade.php
@@ -1,9 +1,12 @@
+{{--
+  Template Name: New Template
+--}}
+
 @extends('layouts.new')
 
 @section('content')
   @while(have_posts()) @php the_post() @endphp
     @include('partials.page-header')
     @include('partials.content-page')
-    blahhhhhhhhhh
   @endwhile
 @endsection
